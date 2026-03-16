7ed85114c39776b1dc15d9ac246d3a048bcf5e67
SoyAdrianYT001 <100530918+El77373883@users.noreply.github.com>
2026-03-16 02:12:51 -0600

    Update Arena.java

--- src/main/java/me/adrian/paintball/game/Arena.java
+++ src/main/java/me/adrian/paintball/game/Arena.java
@@ -1,14 +1,15 @@
 package me.adrian.paintball.game;
 
 import org.bukkit.Location;
+
 import java.util.HashMap;
 import java.util.Map;
 
 public class Arena {
 
     private final String name;
-    private final Map<String, Location> spawns = new HashMap<>();
-    private int maxTeams = 2; // default
+    private int maxTeams = 2; // por defecto
+    private final Map<String, Location> spawns = new HashMap<>(); // RED, BLUE, PINK, GREEN
 
     public Arena(String name) {
         this.name = name;
@@ -18,19 +19,25 @@ public class Arena {
         return name;
     }
 
+    public int getMaxTeams() {
+        return maxTeams;
+    }
+
+    public void setMaxTeams(int maxTeams) {
+        if (maxTeams == 2 || maxTeams == 4) {
+            this.maxTeams = maxTeams;
+        }
+    }
+
     public void setSpawn(String team, Location loc) {
-        spawns.put(team, loc);
+        spawns.put(team.toUpperCase(), loc);
     }
 
     public Location getSpawn(String team) {
-        return spawns.get(team);
+        return spawns.get(team.toUpperCase());
     }
 
-    public void setMaxTeams(int max) {
-        this.maxTeams = max;
-    }
-
-    public int getMaxTeams() {
-        return maxTeams;
+    public Map<String, Location> getSpawns() {
+        return spawns;
     }
 }
