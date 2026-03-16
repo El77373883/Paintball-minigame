d53f855b6417a2e84881ad334bafa250b09064c7
SoyAdrianYT001 <100530918+El77373883@users.noreply.github.com>
2026-03-15 23:42:08 -0600

    Update PaintballPlugin.java

--- src/main/java/me/adrian/paintball/PaintballPlugin.java
+++ src/main/java/me/adrian/paintball/PaintballPlugin.java
@@ -1,1 +1,30 @@
+package me.adrian.paintball;
 
+import me.adrian.paintball.command.PaintballCommand;
+import me.adrian.paintball.game.GameManager;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PaintballPlugin extends JavaPlugin {
+
+    private GameManager gameManager;
+
+    @Override
+    public void onEnable() {
+        this.gameManager = new GameManager();
+
+        if (getCommand("paintball") != null) {
+            getCommand("paintball").setExecutor(new PaintballCommand(gameManager));
+        }
+
+        getLogger().info("PaintballMinigame enabled! Created by soyadrianyt001");
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().info("PaintballMinigame disabled!");
+    }
+
+    public GameManager getGameManager() {
+        return gameManager;
+    }
+}
