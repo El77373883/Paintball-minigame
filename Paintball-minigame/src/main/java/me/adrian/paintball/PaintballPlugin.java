fa09c3f8874b695b781242fede80373f53ff5e67
SoyAdrianYT001 <100530918+El77373883@users.noreply.github.com>
2026-03-16 02:47:07 -0600

    Update PaintballPlugin.java

--- Paintball-minigame/src/main/java/me/adrian/paintball/PaintballPlugin.java
+++ Paintball-minigame/src/main/java/me/adrian/paintball/PaintballPlugin.java
@@ -1,39 +1,64 @@
 package me.adrian.paintball;
 
-import me.adrian.paintball.command.PaintballAdminCommand;
 import me.adrian.paintball.command.PaintballCommand;
+import me.adrian.paintball.command.PaintballAdminCommand;
+import me.adrian.paintball.events.PaintballEvents;
 import me.adrian.paintball.game.GameManager;
-import me.adrian.paintball.listener.PaintballListener;
+import me.adrian.paintball.gui.PaintballPanel;
+import me.adrian.paintball.gui.PaintballShop;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class PaintballPlugin extends JavaPlugin {
 
+    private static PaintballPlugin instance;
     private GameManager gameManager;
+    private PaintballPanel panel;
+    private PaintballShop shop;
 
     @Override
     public void onEnable() {
-        // Inicializar GameManager
-        gameManager = new GameManager(this);
+        instance = this;
+
+        // Inicializamos GameManager y le pasamos la instancia del plugin
+        this.gameManager = new GameManager(this);
+
+        // Inicializamos GUI
+        this.panel = new PaintballPanel(this);
+        this.shop = new PaintballShop(this);
 
         // Registrar comandos
-        this.getCommand("paintball").setExecutor(new PaintballCommand(gameManager));
-        this.getCommand("pb").setExecutor(new PaintballAdminCommand(gameManager));
-        this.getCommand("pa").setExecutor(new PaintballAdminCommand(gameManager));
+        this.getCommand("pa").setExecutor(new PaintballAdminCommand(this));
+        this.getCommand("paintball").setExecutor(new PaintballCommand(this));
 
-        // Registrar listeners
-        getServer().getPluginManager().registerEvents(new PaintballListener(gameManager), this);
+        // Registrar eventos
+        getServer().getPluginManager().registerEvents(new PaintballEvents(this), this);
 
-        // Mensaje de inicio
-        getLogger().info("§aPaintball Plugin habilitado!");
+        // Mensaje de activación
+        getLogger().info("PaintballPlugin activado correctamente!");
     }
 
     @Override
     public void onDisable() {
-        // Mensaje de apagado
-        getLogger().info("§cPaintball Plugin deshabilitado!");
+        getLogger().info("PaintballPlugin desactivado.");
     }
 
+    // Getter de instancia del plugin
+    public static PaintballPlugin getInstance() {
+        return instance;
+    }
+
+    // Getter del GameManager
     public GameManager getGameManager() {
         return gameManager;
+    }
+
+    // Getter del panel
+    public PaintballPanel getPanel() {
+        return panel;
+    }
+
+    // Getter de la tienda
+    public PaintballShop getShop() {
+        return shop;
     }
 }
