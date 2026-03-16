c9ccfe96bf0cb4a1c20d1123fca29d33fc285d50
SoyAdrianYT001 <100530918+El77373883@users.noreply.github.com>
2026-03-15 23:24:43 -0600

    Update PaintballCommand.java

--- PaintballCommand.java
+++ PaintballCommand.java
@@ -1,1 +1,16 @@
+package me.adrian.paintball;
 
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PaintballPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        getLogger().info("PaintballMinigame enabled! Created by soyadrianyt001");
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().info("PaintballMinigame disabled!");
+    }
+}
